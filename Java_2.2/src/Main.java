cc4b905527c98bc797a3107e63299d561a0ee5b0
Ilmira Kudryavtseva <ilshakudz@gmail.com>
2022-02-04 14:46:54 +0400

    bonus added

--- Java_2.2/src/Main.java
+++ Java_2.2/src/Main.java
@@ -1,12 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        int account = 10;
-        int refill = 901;
+        int account = 100;
+        int refill = 100;
         int priceForRub = 100;
 
         boolean isParticipant = account + refill > 1000 ? true : false;
         int finalAccount = isParticipant ? refill / priceForRub + account + refill : account + refill;
-
+        int bonus = isParticipant ? refill / priceForRub : 0;
         System.out.println("На вашем счете: " + finalAccount);
+        System.out.println("Количество начисленных бонусов: " + bonus);
     }
 }
