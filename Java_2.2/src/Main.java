b4c05001a00e0c09ccb8b0f9909adbf27b8153c0
Ilmira Kudryavtseva <ilshakudz@gmail.com>
2022-02-11 19:04:12 +0300

    isParticipant changed

--- Java_2.2/src/Main.java
+++ Java_2.2/src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         int account = 100;
-        int refill = 100;
+        int refill = 1100;
         int priceForRub = 100;
 
-        boolean isParticipant = account + refill > 1000 ? true : false;
+        boolean isParticipant = refill > 1000 ? true : false;
         int finalAccount = isParticipant ? refill / priceForRub + account + refill : account + refill;
         int bonus = isParticipant ? refill / priceForRub : 0;
         System.out.println("На вашем счете: " + finalAccount);
