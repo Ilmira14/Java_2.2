69d6cbc92acd4bd4f13d71059343137d97a250da
Ilmira Kudryavtseva <ilshakudz@gmail.com>
2022-02-19 13:11:49 +0300

    condition fixed

--- Java_2.2/src/Main.java
+++ Java_2.2/src/Main.java
@@ -1,12 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         int account = 100;
-        int refill = 1100;
+        int refill = 1001;
         int priceForRub = 100;
 
         boolean isParticipant = refill > 1000 ? true : false;
-        int finalAccount = isParticipant ? refill / priceForRub + account + refill : account + refill;
         int bonus = isParticipant ? refill / priceForRub : 0;
+        int finalAccount = account + bonus + refill;
         System.out.println("На вашем счете: " + finalAccount);
         System.out.println("Количество начисленных бонусов: " + bonus);
     }
